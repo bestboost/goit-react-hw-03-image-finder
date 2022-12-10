d836fde7f887e538a3f97ff1d5b2315cea69d1b2
Darya Hudzovska <1bestboost@gmail.com>
2022-12-10 21:40:12 +0200

    mute propTypes

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import {Search, Form, SearchButton, SearchLabel, SearchInput} from './Searchbar.styled'
 
 class Searchbar extends Component {
