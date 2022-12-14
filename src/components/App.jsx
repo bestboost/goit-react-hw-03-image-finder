ea16cd6f63202c3deaa3db65a50bdcd76657309b
Darya Hudzovska <1bestboost@gmail.com>
2022-12-14 20:25:55 +0200

    upgrade loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,18 +18,12 @@ class App extends Component  {
         loading: false,
         showModal: false,
         inputValue: '',
+        error: null,
  };
 
 
 //  {id: '', webformatURL: '', largeImageURL: ''} 
- componentDidMount() {
- 
-  //  fetch('https://pixabay.com/api/?q=cat&page=1&key=29692752-5f9a27c26e6deec7970509d3f&image_type=photo&orientation=horizontal&per_page=12')
-  //     .then(res => res.json())
-  //     .then(apiImages => this.setState({apiImages: apiImages.hits}))
-  //     .finally(() => this.setState({loading: false}));
-     
-  };
+
 
   componentDidUpdate(prevProps, prevState) {
 
@@ -37,12 +31,21 @@ class App extends Component  {
      const nextValue = this.state.inputValue;
 
     if (prevValue !== nextValue) {
-      this.setState({loading: true, apiImages: ''})
+      this.setState({loading: true})
       console.log('change value');
       
       fetch(`https://pixabay.com/api/?q=${nextValue}&page=1&key=29692752-5f9a27c26e6deec7970509d3f&image_type=photo&orientation=horizontal&per_page=12`)
-      .then(res => res.json())
+      .then(response => {
+        if(response.ok) {
+          return response.json();
+        }
+
+        return Promise.reject(
+          new Error(`Upsss, no image ${nextValue}!`),
+        );
+      })
       .then(apiImages => this.setState({apiImages: apiImages.hits})) 
+      .catch(error => this.setState({error}))
       .finally(() => this.setState({loading: false}));
     }
   }
@@ -60,7 +63,7 @@ class App extends Component  {
 
 
  render() {
-  const {apiImages, showModal, loading} = this.state;
+  const {apiImages, showModal, loading, error} = this.state;
   const datas = apiImages
   
 console.log(datas)
@@ -72,14 +75,15 @@ console.log(datas)
         gridGap: '16',
         paddingBottom: '24',
       }}
-    > 
+    >  
       <Searchbar onSearch={this.formSubmit}/>
-      
-      {apiImages && <ImageGallery>
-        <ImageGalleryItem onClick={this.toggleModal}/>
-        <LoderButton/>
+    
+      {apiImages  && <ImageGallery>
+           <ImageGalleryItem onClick={this.toggleModal}/>
+           <LoderButton/>
       </ImageGallery>
       }
+       {error && <h1>{error.message}</h1>}
       {loading && <Loader/>} 
       {showModal && <ModalWindow onClose={this.toggleModal}/>}
       <ToastContainer autoClose={3000} position="top-center"/>
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Audio } from 'react-loader-spinner';
-
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 const Loader = () => {  
-    return(        <Audio
-  height="80"
-  width="80"
-  radius="9"
-  color="green"
-  ariaLabel="loading"
-  wrapperStyle
-  wrapperClass
+    return(       
+       <Audio 
     
+  height="40"
+  width="40"
+  radius="9"
+  color= "blue"
+  ariaLabel="loading"
+  wrapperStyle={{margin: 'auto'}}
 />
     )
 };
