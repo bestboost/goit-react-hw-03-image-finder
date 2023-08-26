b63c759cb3fd32e27ca0557119583e2c4914add3
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-08-26 18:11:40 +0300

    -

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,67 +1,69 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {Search, Form, SearchButton, SearchLabel, SearchInput} from './Searchbar.styled'
-import {ReactComponent as SearchIcon} from '../icons/serch.svg';
+import {
+  Search,
+  Form,
+  SearchButton,
+  SearchLabel,
+  SearchInput,
+} from './Searchbar.styled';
+import { ReactComponent as SearchIcon } from '../icons/serch.svg';
 import { toast } from 'react-toastify';
 
-
 class Searchbar extends Component {
-
-      static propTypes = {
-        onClick: PropTypes.func,
-        children: PropTypes.node,
- };
-
- deafultProps = {
-  onClick: () => null,
-  children: null,
-}
-
-state = {
-  inputValue:'',
-};
-
-inputChange = e => {
-  this.setState({inputValue: e.currentTarget.value.trim().toLowerCase()});
-}
-
-handleSubmit = e => {
-  e.preventDefault();
-  const {inputValue} = this.state;
-
-  if (inputValue.trim() === ''){
-    toast.info('Input search word, please');
-    return
-  }
-
-  this.props.onSearch(inputValue);
-  this.setState({inputValue: ''});
-};
-
-render (){
-
-    return (
-        <Search>
-  <Form onSubmit={this.handleSubmit}>
-    <SearchButton type="submit"  aria-label="Find images">{this.props.children}
-      <SearchIcon width="20" height="20" fill="#5b5b5b"/>
-        <SearchLabel >Search</SearchLabel>
-    </SearchButton>
-
-    <SearchInput
-      type="text"
-      autocomplete="off"
-      autoFocus
-      placeholder="Search images and photos"
-      value={this.state.inputValue}
-      onChange={this.inputChange}
-    />
-  </Form>
-</Search>
-
-
-    )
+  static propTypes = {
+    onClick: PropTypes.func,
+    children: PropTypes.node,
   };
-};
+
+  deafultProps = {
+    onClick: () => null,
+    children: null,
+  };
+
+  state = {
+    inputValue: '',
+  };
+
+  inputChange = e => {
+    this.setState({ inputValue: e.currentTarget.value.trim().toLowerCase() });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { inputValue } = this.state;
+
+    if (inputValue.trim() === '') {
+      toast.info('Input search word, please');
+      return;
+    }
+
+    this.props.onSearch(inputValue);
+    this.setState({ inputValue: '' });
+  };
+
+  render() {
+    return (
+      <Search>
+        <Form onSubmit={this.handleSubmit}>
+          <SearchButton type="submit" aria-label="Find images">
+            {this.props.children}
+            <SearchIcon width="20" height="20" fill="#5b5b5b" />
+            <SearchLabel>Search</SearchLabel>
+          </SearchButton>
+
+          <SearchInput
+            type="text"
+            autocomplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.inputValue}
+            onChange={this.inputChange}
+          />
+        </Form>
+      </Search>
+    );
+  }
+}
 
 export default Searchbar;
